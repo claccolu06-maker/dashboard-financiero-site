d6a20af03bd44a8c5ee481da50223fd546f5d076
claccolu06-maker <claccolu06@gmail.com>
2026-01-16 19:35:24 +0100

    Add files via upload

---
+++ index.html.py
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="vewport" content="wdth=devce-wdth, ntal-scale=1.0" />
+  <ttle>Dashboard Fnancero</ttle>
+  <lnk rel="stylesheet" href="style.css" />
+</head>
+<body>
+  <header class="hero-df">
+    <dv class="contaner">
+      <dv class="hero-df-content">
+        <h1>Ten claro tu dnero sn pelearte con Excel</h1>
+        <p class="hero-df-subttle">
+          Dashboard Fnancero reúne en un solo sto tus ngresos, gastos y ahorros, con gráfcos claros y automatzados,
+          para que veas en segundos a dónde se va tu dnero.
+        </p>
+        <dv class="hero-df-actons">
+          <a href="https://www.dashboardfnancero.es/" class="btn-prmary-df">
+            Acceder a m dashboard
+          </a>
+          <a href="#ejemplo-panel" class="btn-secondary-df">
+            Ver ejemplo de panel
+          </a>
+        </dv>
+      </dv>
+    </dv>
+  </header>
+
+  <!-- Aquí luego añadremos el resto de seccones -->
+</body>
+</html>
